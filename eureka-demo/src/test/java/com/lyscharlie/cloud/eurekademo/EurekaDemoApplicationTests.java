b160cb110eea91597732f69cca47b3afdad38ebc
lycharlie <lyscharlie@hotmail.com>
2020-05-28 18:27:50 +0800

    111

---
+++ eureka-demo/src/test/java/com/lyscharlie/cloud/eurekademo/EurekaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyscharlie.cloud.eurekademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
